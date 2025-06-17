9fc626d3f878d978658f611895960cb91cf2de27
ValenDuarte1918 <valentinduarte1@hotmail.com>
2025-06-17 14:19:44 -0300

    Añadi la clase AccesoDatos para gestión de conexiones SQL

---
+++ Datos/AccesoDatos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class AccesoDatos
+    {
+        private string connectionString = "Data Source=localhost\\sqlexpress;Initial Catalog=ClinicaMedica;Integrated Security=True";
+
+        private SqlConnection connection()
+        {
+            SqlConnection cnxn = null;
+            try
+            {
+                cnxn = new SqlConnection(connectionString);
+                cnxn.Open();
+                return cnxn;
+            }
+            catch (Exception ex)
+            {
+                try
+                {
+                    string AuxConnectionString = connectionString.Replace("\\sqlexpress", "");
+                    cnxn = new SqlConnection(AuxConnectionString);
+                    cnxn.Open();
+                    return cnxn;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
