ddc4ead3d4918ad94c68495b628f47345c6bfc0d
camimoy <camimoyano2006@gmail.com>
2025-07-15 21:47:12 -0300

    cami commit
    SeguimientosPaciente: Comentarios e Historial a los pacientes (ingreso de comentario y guardado)

--- ClinicaMedica/SeguimientosPacientes.aspx.designer.cs
+++ ClinicaMedica/SeguimientosPacientes.aspx.designer.cs
@@ -69,6 +69,15 @@ namespace ClinicaMedica
         protected global::System.Web.UI.WebControls.Label lblNombrePaciente;
 
         /// <summary>
+        /// lblNombrePaciente1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombrePaciente1;
+
+        /// <summary>
         /// lblFechaTurno control.
         /// </summary>
         /// <remarks>
